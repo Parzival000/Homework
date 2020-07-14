72e040816bdfcdd396d0cd18988cba2556e13b66
Parzival000 <palm559@hotmail.com>
2020-07-14 14:55:21 +0700

    initial commit

---
+++ work1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+int main()
+{
+std::cout << std :: endl;
+std::cout << "1st Term of 2020         /45132114'    saharat pinyahan"<<std :: endl;
+std::cout << std :: endl;
+std::cout << std :: endl;
+std::cout << "--------------------------------------------------------------------------------"<<std :: endl;
+std::cout << "| Subject ID | Subject name			  | Score (100)| Grad |"<<std :: endl;
+std::cout << "--------------------------------------------------------------------------------"<<std :: endl;
+std::cout << "|  4570034   | Introduction to computer  |    85      | 'A'  |"<<std :: endl;
+std::cout << "|  4580247   | English                   |    74      | 'B'  |"<<std :: endl;
+std::cout << "|  4530001   | Desing Thinking           |    92      | 'A'  |"<<std :: endl;
+std::cout << "--------------------------------------------------------------------------------"<<std :: endl;
+std::cout << "|  TOTAL     | Subject = 3               | Average = 3.87 |"<<std :: endl;
+std::cout << "===================================*===="<<std :: endl;
+std::cout << std :: endl;
+std::cout << std :: endl;
+std::cout << std :: endl;
+std::cout << std :: endl;
+std::cout << "KMUTNB"<<std :: endl;
+return 0;
+}
