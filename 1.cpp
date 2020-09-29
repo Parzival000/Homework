a08303705b8c712701d23ebda55efe85b3b26687
Parzival000 <68264822+Parzival000@users.noreply.github.com>
2020-09-29 12:28:38 +0700

    Add files via upload

--- 1.cpp
+++ 1.cpp
@@ -1,143 +1,128 @@
-/*#include <iostream>
-using namespace std;
-int swap(int a ,int b);
-int main()
-{
-	int x =5, y=3;
-	cout<<swap(x , y) <<endl;
-	cout<<"x = " << x <<endl;
-	cout<<"y = " << y <<endl;
-	return 0;
-}
-int swap(int a ,int b)
-{
-	a = 6;
-	b = 5;
-	return a+b;
-}*/
-
 /*#include<iostream>
 #include<string>
 using namespace std;
-void ParseName(string &First , string &Last , const string FullName);
+int a[5];
+string name[5];
+
 int main()
 {
-	string Name;
-	string LastName , FirstName;
-	char sub;
-	cout << "Enter String : ";
-	cin >> Name;
-	cout << "Enter Substring : ";
-	cin >> sub;
-	ParseName(FirstName , LastName , Name);
-	Name = FirstName + " " + LastName;
-	cout << "Name : " << Name << endl;
-	return 0;
-}
-void ParseName(string &First , string &Last , const string FullName)
-{
-	int I = FullName.find(",");
-	Last = FullName.substr(0 , I);
-	First = FullName.substr(I + 2 , FullName.size());
+	
+	for(int x= 0; x<5; x++)
+	{
+		cout<<"Enter name ["<<x+1<<"] : ";
+		cin >>name[x];
+		cout<<"Enter score ["<<x+1<<"] : ";
+		cin >>a[x];
+		cout<<endl;
+		
+	}
+	cout<<"No. \tName \tscore"<<endl;
+	cout<<setw(50)<<setfill"-"<<endl;
+	for(int x = 0; x < 5 ; x++)
+	{
+		cout<<x+1<<"\t";
+		cout<<name[x]<<"\t"<<a[x]<<endl;
+
+	}
+
+
 }*/
+
+
 
 /*#include<iostream>
 #include<string>
+#include<array>
 using namespace std;
-void ParseName(string &str1 , string &str2 ,string &str, const string sub_str);
+
 int main()
 {
-	string str,sub_str;
-	string str1, str2;
-	cout<<"Enter string :";
-	cin>>str;
-	cout<<"Enter Substring :";
-	cin>>sub_str;
-	ParseName(str1 , str2,str , sub_str);
-	cout<<"String 1 = "<<str1<<endl;
-	cout << "String2 = " << str2 << endl;
-	return(0);
 
-}
-void ParseName(string &str1 , string &str2,string &str , const string sub_str)
+	int a[4] ={-1,6,9,2};
+	int max = a[0];
+	int min = a[0];
+	int sum
+	for(int i=1;i<4;i++){
+		if(a[i]>max)
+		 max =a[i];
+		if(a[i]<max)
+		 min =a[i];
+	}
+	cout<<"Maximum value is "<< max <<endl;
+	cout<<"Mainimum value is "<< max <<endl;
+	int n = sizeof(a) / sizeof(a[0]);
+	sort(a,a+n);
+	cout <<"Sorted Array :";
+	for(int i = 0 ; i < n ;i++)
+	{ cout<< a[i]<<" ";
+	 cout << sum += a[i] << endl;
+	}
+	return 0;
+	
+}*/
+
+
+/*#include<iostream>
+#include<string>
+#include<array>
+using namespace std;
+float average(float num[10]);
+int main()		
 {
-	int I = str.find(sub_str);
-	str1 = str.substr(0 , I);
-	str2 = str.substr(I + 2 ,str.size());
+	int i; float score[10],avg_score;
+	for(i=0;i<10;i++)
+	 cout<< "Enter score ["<<i<<"]";
+	cin >> score[i];
+	avg_score = average(score);
+	cout <<"Average score is "<<avg_score <<endl;
+	}
+	return 0;
+}
+float average(float num[10])
+
+{
+	float total = 0.0 , avg = 0.0;
+	for(int i=0; i<10;i ++)
+	{
+		total =total+num[i];
+		avg = total /10;
+	}
+	return avg;
+
 }*/
 
 
 #include<iostream>
-#include<string>
 #include<iomanip>
 using namespace std;
-void DisplayMenu();
-float Area(const float Radius);
-float Area(const float Length,const float widht);
-double Area(const double based ,const double high);
+void display(int salary[] , int num);
+int cal_bonus(int salary);
 int main()
 {
-	char Choice;
-	bool Flag =true;
-	do{
-		DisplayMenu();
-		cin>>Choice;
-		if (Choice == '1'){
-			float Radius;
-			cout<<"\nEnter radius : ";
-			cin>>Radius;
-			cout<<"Area of Circle = "<< fixed;
-			cout<< setprecision(2)<< Area(Radius)<<endl;
-		}
-		else if(Choice == '2'){
-			float Length,Widht;
-			cout <<"Enter Length and width :";
-			cin >>Length>>Widht;
-			cout <<"Area of Rectangle = " <<fixed;
-			cout <<setprecision(2)<<Area(Length,Widht);
-		}
-		else if(Choice == '3'){
-			double based ,high;
-			cout <<"Enter Length and based :";
-			cin >>based>>high;
-			cout <<"Area of Rectangle = " <<fixed;
-			cout <<setprecision(2)<<Area(based,high);
-		}
-		else if (Choice =='4') Flag = false;
-		else {
-			cout <<"\nYou choose out of range is";
-			cout <<"not process.\n";
-		}
-	}while (Flag);
-	cout<<"\n. . .Exit Program . . .\n";
-	return(0);
+	int num , bonus;
+	cout << "Enter Number of Person : ";
+	cin >> num;
+	int *salary = new int[num];
+	for(int i = 0 ; i < num ; i++)
+	{
+		cout << "Enter the salary "<< i+1 << " : ";
+		cin >> salary[i];
+	}
+	bonus = cal_bonus(*salary);
+	display(salary , num);
 }
-float Area(const float Radius)
+int cal_bonus(int salary)
 {
-	return(3.14159F *Radius *Radius);
+	int total;
+	total = salary * 2;
+	return (total);
 }
-float Area(const float Length,const float widht)
+void display(int salary[] , int num)
 {
-	return(Length *widht);
+	cout << fixed << setw(80) << setfill('-') << "\n";
+	cout << "There are " << num << " person" << endl;
+	for(int i = 0 ; i < num ; i++)
+	{
+		cout << "The salary for person " << i+1 << " =" << salary[i] << " and Bonus = " <<cal_bonus(salary[i])<< endl;
+	}
 }
-double Area(const double based ,const double high)
-{
-	return(0.5 *(based *high));
-}
-void DisplayMenu()
-{
-	cout<<endl;
-	cout<<"Program Calulate Area"<<endl;
-	cout<<"1.Clrcle"<<endl;
-	cout<<"2.Rectangle"<<endl;
-	cout<<"3.Trlangle"<<endl;
-	cout<<"4.Exit"<<endl;
-	cout<<"Enter your choose number: ";
-}
-
-
-
-	
-
-
-
