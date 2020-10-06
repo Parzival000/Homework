27c96153b6c27b5a5552f52dd57e226c948a5fef
Parzival000 <68264822+Parzival000@users.noreply.github.com>
2020-10-06 21:45:09 +0700

    Add files via upload

--- 1.cpp
+++ 1.cpp
@@ -1,128 +1,34 @@
-/*#include<iostream>
-#include<string>
-using namespace std;
-int a[5];
-string name[5];
-
-int main()
-{
-	
-	for(int x= 0; x<5; x++)
-	{
-		cout<<"Enter name ["<<x+1<<"] : ";
-		cin >>name[x];
-		cout<<"Enter score ["<<x+1<<"] : ";
-		cin >>a[x];
-		cout<<endl;
-		
-	}
-	cout<<"No. \tName \tscore"<<endl;
-	cout<<setw(50)<<setfill"-"<<endl;
-	for(int x = 0; x < 5 ; x++)
-	{
-		cout<<x+1<<"\t";
-		cout<<name[x]<<"\t"<<a[x]<<endl;
-
-	}
-
-
-}*/
-
-
-
-/*#include<iostream>
-#include<string>
-#include<array>
-using namespace std;
-
-int main()
-{
-
-	int a[4] ={-1,6,9,2};
-	int max = a[0];
-	int min = a[0];
-	int sum
-	for(int i=1;i<4;i++){
-		if(a[i]>max)
-		 max =a[i];
-		if(a[i]<max)
-		 min =a[i];
-	}
-	cout<<"Maximum value is "<< max <<endl;
-	cout<<"Mainimum value is "<< max <<endl;
-	int n = sizeof(a) / sizeof(a[0]);
-	sort(a,a+n);
-	cout <<"Sorted Array :";
-	for(int i = 0 ; i < n ;i++)
-	{ cout<< a[i]<<" ";
-	 cout << sum += a[i] << endl;
-	}
-	return 0;
-	
-}*/
-
-
-/*#include<iostream>
-#include<string>
-#include<array>
-using namespace std;
-float average(float num[10]);
-int main()		
-{
-	int i; float score[10],avg_score;
-	for(i=0;i<10;i++)
-	 cout<< "Enter score ["<<i<<"]";
-	cin >> score[i];
-	avg_score = average(score);
-	cout <<"Average score is "<<avg_score <<endl;
-	}
-	return 0;
-}
-float average(float num[10])
-
-{
-	float total = 0.0 , avg = 0.0;
-	for(int i=0; i<10;i ++)
-	{
-		total =total+num[i];
-		avg = total /10;
-	}
-	return avg;
-
-}*/
-
-
 #include<iostream>
+#include<string>
 #include<iomanip>
 using namespace std;
-void display(int salary[] , int num);
-int cal_bonus(int salary);
-int main()
-{
-	int num , bonus;
-	cout << "Enter Number of Person : ";
-	cin >> num;
-	int *salary = new int[num];
-	for(int i = 0 ; i < num ; i++)
-	{
-		cout << "Enter the salary "<< i+1 << " : ";
-		cin >> salary[i];
-	}
-	bonus = cal_bonus(*salary);
-	display(salary , num);
+void display(string name[5], float price[3][5]){
+ for (int i = 0; i<5; i++){
+   cout << i+1 <<setw(10)<< name[i] <<setw(20)<< price[0][i];
+   cout <<setw(20)<< price[1][i] <<setw(24)<< price[2][i] << endl;
+  }
 }
-int cal_bonus(int salary)
-{
-	int total;
-	total = salary * 2;
-	return (total);
+float calprice(float price[3][5]){
+ for(int i=0;i<5;i++){
+ price[1][i] = price[0][i]*(0.07);
+ price[2][i] = price[0][i] + price[1][i];
+ }
+ return (price[3][5]);
 }
-void display(int salary[] , int num)
-{
-	cout << fixed << setw(80) << setfill('-') << "\n";
-	cout << "There are " << num << " person" << endl;
-	for(int i = 0 ; i < num ; i++)
-	{
-		cout << "The salary for person " << i+1 << " =" << salary[i] << " and Bonus = " <<cal_bonus(salary[i])<< endl;
-	}
+string name[5];
+float price[3][5];
+int main(){
+ for (int i = 0; i < 5; i++){
+  cout << "Enter Product Name : ";
+  cin >> name[i];
+  cout << "\t price : ";
+  cin >> price[0][i];
+ }
+ //Calculate
+ cout << setfill(' ') <<"No."<<setw(10)<<"Product"<<setw(20) <<"price"<<setw(20)<< "vat7%" << setw(24) <<"Net" << endl;
+ calprice(price);
+ display(name,price);
+
+ system("pause");
+ return 0;
 }
